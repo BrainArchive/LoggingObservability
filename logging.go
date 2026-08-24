7454fed2333e144d89c6367e71db3b587c2b0ae6
BrainArchive <zekeflores.aer@gmail.com>
2026-08-24 21:55:55 +0800

    implemented stack traces, multiple error handling creating errors with attributes

--- logging.go
+++ logging.go
@@ -2,17 +2,72 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	"boot.dev/linko/internal/linkoerr"
+	pkgerr "github.com/pkg/errors"
 )
 
+type stackTracer interface {
+	error
+	StackTrace() pkgerr.StackTrace
+}
+
+type multiError interface {
+	error
+	Unwrap() []error
+}
+
 type closeFunc func() error
+
+func replaceAttr(groups []string, a slog.Attr) slog.Attr {
+	if a.Key == "error" {
+		err, ok := a.Value.Any().(error)
+		if !ok {
+			return a
+		}
+
+		if multiErr, ok := errors.AsType[multiError](err); ok {
+			var errAttrs []slog.Attr
+			for i, err := range multiErr.Unwrap() {
+				attrs := errorAttrs(err)
+				errAttrs = append(errAttrs, slog.GroupAttrs(fmt.Sprintf("error_%d", i+1), attrs...))
+			}
+			return slog.GroupAttrs("errors", errAttrs...)
+		}
+
+		attrs := errorAttrs(err)
+		return slog.GroupAttrs("error", attrs...)
+	}
+
+	return a
+}
+
+func errorAttrs(err error) []slog.Attr {
+	var attrs []slog.Attr
+	attrs = append(attrs, slog.Attr{
+		Key:   "message",
+		Value: slog.StringValue(err.Error()),
+	})
+	if stackErr, ok := errors.AsType[stackTracer](err); ok {
+		attrs = append(attrs, slog.Attr{
+			Key:   "stack_trace",
+			Value: slog.StringValue(fmt.Sprintf("%+v", stackErr.StackTrace())),
+		})
+	}
+	attrs = append(attrs, linkoerr.Attrs(err)...)
+	return attrs
+}
 
 func initializeLogger() (*slog.Logger, closeFunc, error) {
 	logFile := os.Getenv("LINKO_LOG_FILE")
 	debugHandler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-		Level: slog.LevelDebug,
+		Level:       slog.LevelDebug,
+		ReplaceAttr: replaceAttr,
 	})
 	if logFile == "" {
 		// function isn't nil so as to make sure it's callable.
@@ -28,7 +83,8 @@ func initializeLogger() (*slog.Logger, closeFunc, error) {
 
 	bufferedWriter := bufio.NewWriterSize(file, 8192)
 	infoHandler := slog.NewJSONHandler(bufferedWriter, &slog.HandlerOptions{
-		Level: slog.LevelInfo,
+		Level:       slog.LevelInfo,
+		ReplaceAttr: replaceAttr,
 	})
 	logger := slog.New(slog.NewMultiHandler(
 		debugHandler,
